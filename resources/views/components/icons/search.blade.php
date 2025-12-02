85283d88c6b791e4d60aa3c7821b1a73030c226c
tqt97 <kutuanonline199@gmail.com>
2025-12-02 20:47:38 +0700

    Fix class size icon

--- resources/views/components/icons/search.blade.php
+++ resources/views/components/icons/search.blade.php
@@ -1,6 +1,6 @@
 @props(['size' => 4])
 
 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
-    {{ $attributes->merge(['class' => 'size-' . $size]) }}>
+    {{ $attributes->merge(['class' => 'w-' . $size.' h-' . $size]) }}>
     <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
 </svg>
